172f5bdce0cd735b11a8ab5db641a7f7a292a35c
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2020-02-24 14:21:19 +0100

    STDEV(Z) algorithm implementation

--- algorithms.py
+++ algorithms.py
@@ -1,4 +1,5 @@
 import math
+import statistics
 
 import helper_scripts.helpers as helpers
 
@@ -27,11 +28,21 @@ def z_diff(data, threshold):
             continue
 
         diff = (survey[5]-prev_survey[5])/(survey[8]-prev_survey[8]).total_seconds()
-        print(abs(diff))
+        # print(abs(diff))
         if abs(diff) > threshold:
             result.append(survey[6:8])
     # print("{0}".format(result))
         prev_survey = survey
+    return result
+
+
+def stdev_alg(data, threshold, window_size):
+    result = list()
+    for index, survey in data[window_size:].iterrows():
+        zaxis_data_window = list(data.iloc[list(range(index-window_size, index)), 5])
+        stdev_val = statistics.stdev(zaxis_data_window)
+        if stdev_val > threshold:
+            result.append(survey[6:8])
     return result
 
 
