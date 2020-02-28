4c7215d210ab0174d8ab0adf76ba2df22aa6e856
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2020-02-28 16:58:24 +0100

    Changed height of survey list and added scrollbar

--- frontend/radac-frontend/src/components/SurveyList.jsx
+++ frontend/radac-frontend/src/components/SurveyList.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Typography from "@material-ui/core/Typography";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+        root: {
+            position: 'relative',
+            overflow: 'auto',
+            maxHeight: '70vh',
+        },
+    })
+);
 
 export default function SurveyList(props) {
-
+    const classes = useStyles();
     const surveyNames = props.data.map((element) =>
-            <ListItem key={element.id} button>{element.name}</ListItem>
-        );
+        <ListItem key={element.id} button>
+            <Typography component={"h2"} variant={"h6"} noWrap>
+                {element.name}
+            </Typography>
+        </ListItem>
+    );
 
 
-    return(
-        <List>
+    return (
+        <List className={classes.root}>
+            {surveyNames}
+            {surveyNames}
+            {surveyNames}
+            {surveyNames}
             {surveyNames}
         </List>
     );
