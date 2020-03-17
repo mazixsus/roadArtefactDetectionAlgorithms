837f1c8e96d3b4be2c748d63ce2405bd2eb02fb5
Jakub Mielniczuk <mielniczuk.jakub@gmail.com>
2020-03-17 14:54:30 +0100

    Fixed survey names fetch from  mock API

--- frontend/radac-frontend/src/components/drawer/SurveyList.jsx
+++ frontend/radac-frontend/src/components/drawer/SurveyList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Typography from "@material-ui/core/Typography";
@@ -16,36 +16,39 @@ const useStyles = makeStyles(theme => ({
 );
 
 export default function SurveyList(props) {
+    const [isDataLoaded, setIsDataLoaded] = useState(false);
     useEffect(() => {
-            fetch("/survey/names")
-                .then(res =>
-                    res.json()
-                )
-                .then(json => {
-                    props.surveysInfoFetched(json)
-                });
+            if (!isDataLoaded) {
+                fetch("/survey/names")
+                    .then(res =>
+                        res.json()
+                    )
+                    .then(json => {
+                        props.surveysInfoFetched(json);
+                    })
+                    .then(() => setIsDataLoaded(true));
+            }
+
         }
     );
 
     const classes = useStyles();
-    const surveyNames = props.surveysInfo.map((element) =>
-        <ListItem key={element.surveyId} button>
-            <Typography component={"h2"} variant={"h6"} noWrap>
-                {element.filename}
-            </Typography>
-        </ListItem>
-    );
+    const surveyNames =
+        props.surveysInfo.map((element) =>
+            <ListItem key={element.surveyId} button>
+                <Typography component={"h2"} variant={"h6"} noWrap>
+                    {element.fileName}
+                </Typography>
+            </ListItem>
+        );
 
 
     return (
         <List
             className={classes.root}
         >
-            {surveyNames}
-            {surveyNames}
-            {surveyNames}
-            {surveyNames}
-            {surveyNames}
+            {isDataLoaded && surveyNames
+            }
         </List>
     );
 }
