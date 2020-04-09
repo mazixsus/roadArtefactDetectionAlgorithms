6cd814be4a7bd246be38738580cd171f50b3258a
mateu <mateusz@edu.pl>
2020-04-09 16:04:12 +0200

    backend - endgame

---
+++ backend/roadArtefactDetection/helper_function.py
@@ -0,0 +1,108 @@
+import os
+import signal
+import time
+from threading import Thread
+
+from roadArtefactDetection import algorithms
+from roadArtefactDetection.helper_scripts import helpers
+
+ALGORITHM_NAMES = ["Z-THRESH", "Z-DIFF", "STDEV", "G-ZERO", "MOD-Z-THRESH", "F-THRESH"]
+
+
+def prepare_results(artefacts_positions):
+    surveys = []
+    for artefact in artefacts_positions:
+        surveys.append(
+            {
+                "lat": artefact[0],
+                "lng": artefact[1]
+            }
+        )
+    return surveys
+
+
+def count_time(function, *args):
+    prev_time = time.perf_counter()
+    result = function(*args)
+    realize_time = time.perf_counter() - prev_time
+    return result, realize_time
+
+
+def get_alg_result(data):
+    return {
+        0: count_time(algorithms.z_thresh, data, 1.2),
+        1: count_time(algorithms.z_diff, data, 3),
+        2: count_time(algorithms.stdev_alg, data, 0.25, 50),
+        3: count_time(algorithms.g_zero, data, 0.8),
+        4: count_time(algorithms.mod_z_thresh, data, 4.3),
+        5: count_time(algorithms.f_thresh, data, 50, 1, 1)
+    }
+
+
+def get_statistic(possible_points_grouped, bumps, realize_time):
+    true_positives_points = helpers.true_positives(
+        possible_points_grouped, helpers.bumps_to_tuplepoints(bumps), 20)
+
+    true_positives = len(true_positives_points)
+
+    accuracy = float(0.0)
+    if len(bumps) > 0:
+        accuracy = float(len(true_positives_points) / len(bumps) * 100)
+
+    false_positives = len(possible_points_grouped) - len(true_positives_points)
+    false_negatives = len(bumps) - len(true_positives_points)
+    return {
+        "acc": accuracy,
+        "tp": true_positives,
+        "fp": false_positives,
+        "fn": false_negatives,
+        "time": realize_time
+    }
+
+
+def handle_uploaded_file(path, file):
+    with open(path, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+
+
+def save_files(data_file, bumps_file, data_path, bumps_path):
+    filenames = os.listdir(data_path)
+    counter = 0
+    data_file_name = data_file.name
+    while data_file_name in filenames:
+        counter += 1
+        data_file_name = data_file_name.replace(".csv", "(" + str(counter) + ").csv")
+
+    data_file_path = data_path + data_file_name
+    bumps_file_path = bumps_path + data_file_name.replace('.csv', '(bumps).csv')
+    handle_uploaded_file(data_file_path, data_file)
+    handle_uploaded_file(bumps_file_path, bumps_file)
+
+    return data_file_path, bumps_file_path
+
+
+def run_algorithms(data, bumps, timeout):
+    results = []
+
+    for i in range(0, 6):
+        alg_result = get_alg_result(data)[i]
+        grouped_possible_artefacts = helpers.group_duplicates(alg_result[0], 20, timeout)
+        if grouped_possible_artefacts is not None:
+            error = False
+            statistic = get_statistic(grouped_possible_artefacts, bumps, alg_result[1])
+            detected_bumps = prepare_results(grouped_possible_artefacts)
+        else:
+            error = True
+            statistic = None
+            detected_bumps = None
+
+        algorithm_data = {
+            "algorithmId": i,
+            "algorithmName": ALGORITHM_NAMES[i],
+            "error": error,
+            "detectedBumps": detected_bumps,
+            "stats": statistic
+        }
+        results.append(algorithm_data)
+    return results
--- backend/roadArtefactDetection/views.py
+++ backend/roadArtefactDetection/views.py
@@ -1,129 +1,78 @@
 import os
 import json
-import time
-import roadArtefactDetection.algorithms as algorithms
 import roadArtefactDetection.helper_scripts.helpers as helpers
-from django.views.decorators.csrf import csrf_exempt
-
 import pandas
-from django.shortcuts import render
-from django.http import HttpResponse
 
-ALGORITHM_NAMES = ["Z-THRESH", "Z-DIFF", "STDEV", "G-ZERO", "MOD-Z-THRESH", "F-THRESH"]
+from django.http import HttpResponse, HttpResponseNotFound
+from django.views.decorators.csrf import csrf_exempt
+from roadArtefactDetection.helper_function import run_algorithms, prepare_results, save_files
 
-
-def prepare_results(artefacts_positions):
-    surveys = []
-    for artefact in artefacts_positions:
-        surveys.append(
-            {
-                "lat": artefact[0],
-                "lng": artefact[1]
-            }
-        )
-    return surveys
-
-
-def count_time(function, *args):
-    prev_time = time.perf_counter()
-    result = function(*args)
-    realize_time = time.perf_counter() - prev_time
-    return result, realize_time
-
-
-def get_alg_result(data):
-    return {
-        0: count_time(algorithms.z_thresh, data, 1.2),
-        1: count_time(algorithms.z_diff, data, 3),
-        2: count_time(algorithms.stdev_alg, data, 0.25, 50),
-        3: count_time(algorithms.g_zero, data, 0.8),
-        4: count_time(algorithms.mod_z_thresh, data, 4.3),
-        5: count_time(algorithms.f_thresh, data, 50, 1, 1)
-    }
-
-
-def get_statistic(possible_points_grouped, bumps, realize_time):
-    true_positives_points = helpers.true_positives(
-        possible_points_grouped, helpers.bumps_to_tuplepoints(bumps), 20)
-
-    true_positives = len(true_positives_points)
-
-    accuracy = float(0.0)
-    if len(bumps) > 0:
-        accuracy = float(len(true_positives_points) / len(bumps) * 100)
-
-    false_positives = len(possible_points_grouped) - len(true_positives_points)
-    false_negatives = len(bumps) - len(true_positives_points)
-    return {
-        "acc": accuracy,
-        "tp": true_positives,
-        "fp": false_positives,
-        "fn": false_negatives,
-        "time": realize_time
-    }
-
-
-def run_algorithms(data, bumps):
-    results = []
-
-    for i in range(0, 6):
-        alg_result = get_alg_result(data)[i]
-        grouped_possible_artefacts = helpers.group_duplicates(alg_result[0], 20)
-        statistic = get_statistic(grouped_possible_artefacts, bumps, alg_result[1])
-        detected_bumps = prepare_results(grouped_possible_artefacts)
-        algorithm_data = {
-            "algorithmId": i,
-            "algorithmName": ALGORITHM_NAMES[i],
-            "detectedBumps": detected_bumps,
-            "stats": statistic
-        }
-        results.append(algorithm_data)
-    return results
+DATA_PATH = './roadArtefactDetection/data/'
+BUMPS_PATH = './roadArtefactDetection/bumps/'
+DATA_COL_LABEL = ['X', 'Y', 'Z', 'N', 'E', 'Z2', 'Latitude', 'Longitude', 'Time', 'Speed', 'Course', 'Accuracy']
+BUMPS_COL_LABEL = ['Latitude', 'Longitude']
+TIMEOUT = 5
 
 
 @csrf_exempt
 def add_survey(request):
+
+    if request.method != 'POST' or 'survey' not in request.POST or 'bumps' not in request.POST:
+        return HttpResponse(status=400)
+
     data_file = request.FILES['survey']
     bumps_file = request.FILES['bumps']
-    filenames = os.listdir("./roadArtefactDetection/data/")
     data_file_name = data_file.name
-    counter = 0
-    while data_file_name in filenames:
-        counter += 1
-        data_file_name = data_file_name.replace(".csv", "(" + str(counter) + ").csv")
 
-    file_path = './roadArtefactDetection/data/' + data_file_name
-    bumps_file_path = './roadArtefactDetection/bumps/' + data_file_name.replace('.csv', '(bumps).csv')
-    handle_uploaded_file(file_path, data_file)
-    handle_uploaded_file(bumps_file_path, bumps_file)
+    if not data_file_name.endswith('.csv'):
+        return HttpResponse('File is not csv.', status=400)
+
+    csv_data_file = pandas.read_csv(data_file)
+    csv_bumps_file = pandas.read_csv(bumps_file)
+
+    if not (csv_bumps_file.columns == BUMPS_COL_LABEL).all():
+        return HttpResponse('Bumps file not valid.')
+
+    if not (csv_data_file.columns == DATA_COL_LABEL).all():
+        return HttpResponse('Data file not valid.')
+
+    file_path, bumps_file_path = save_files(data_file, bumps_file, DATA_PATH, BUMPS_PATH)
 
     data = pandas.read_csv(file_path, parse_dates=['Time'])
     bumps = pandas.read_csv(bumps_file_path)
-    result = run_algorithms(data, bumps)
+    result = run_algorithms(data, bumps, TIMEOUT)
 
     return HttpResponse(result)
 
 
-def handle_uploaded_file(path, file):
-    with open(path, 'wb+') as destination:
-        for chunk in file.chunks():
-            destination.write(chunk)
-
-
 def get_results(request):
-    survey_id = request.GET['surveyId']
-    filenames = os.listdir("./roadArtefactDetection/data/")
-    file_path = "./roadArtefactDetection/data/"+filenames[int(survey_id)]
-    bumps_file_path = "./roadArtefactDetection/bumps/"+filenames[int(survey_id)].replace(".csv", "(bumps).csv")
+
+    if request.method != 'GET' or 'surveyId' not in request.GET:
+        return HttpResponse(status=400)
+
+    survey_id_str = request.GET['surveyId']
+
+    try:
+        survey_id = int(survey_id_str)
+    except ValueError:
+        return HttpResponse(status=400)
+
+    filenames = os.listdir(DATA_PATH)
+    file_path = DATA_PATH+filenames[survey_id]
+    bumps_file_path = BUMPS_PATH+filenames[survey_id].replace(".csv", "(bumps).csv")
     data = pandas.read_csv(file_path, parse_dates=['Time'])
     bumps = pandas.read_csv(bumps_file_path)
-    result = run_algorithms(data, bumps)
+    result = run_algorithms(data, bumps, TIMEOUT)
     return HttpResponse(result)
 
 
 def get_survey_names(request):
+
+    if request.method != 'GET':
+        return HttpResponse(status=400)
+
     print(os.listdir("./"))
-    filenames = os.listdir("./roadArtefactDetection/data/")
+    filenames = os.listdir(DATA_PATH)
 
     result = []
     for index, filename in enumerate(filenames, start=0):
@@ -135,8 +84,18 @@ def get_survey_names(request):
 
 
 def get_bumps(request):
-    survey_id = request.GET['surveyId']
-    filenames = os.listdir("./roadArtefactDetection/bumps/")
-    bumps = pandas.read_csv("./roadArtefactDetection/bumps/"+filenames[int(survey_id)])
+
+    if request.method != 'GET' or 'surveyId' not in request.GET:
+        return HttpResponse(status=400)
+
+    survey_id_str = request.GET['surveyId']
+
+    try:
+        survey_id = int(survey_id_str)
+    except ValueError:
+        return HttpResponse(status=400)
+
+    filenames = os.listdir(BUMPS_PATH)
+    bumps = pandas.read_csv(BUMPS_PATH+filenames[survey_id])
     bumps_tuplepoints = helpers.bumps_to_tuplepoints(bumps)
     return HttpResponse(prepare_results(bumps_tuplepoints))
